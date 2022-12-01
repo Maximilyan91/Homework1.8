3dcc3054456b16df850dc706098a6b1fc564bb67
Максим <gmax7911@gmail.com>
2022-12-01 20:19:35 +0300

    разница веса

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ public class Main {
         var weightFighter1 = 78.2;
         var weightFighter2 = 82.7;
         var totalWeight = weightFighter1 + weightFighter2;
-        System.out.println(totalWeight);
+        System.out.println("Общий вес двух бойцов: " + totalWeight + "кг!");
 
         var differenceWeight = weightFighter2 - weightFighter1;
-        System.out.println(differenceWeight);
+        System.out.println("Разница веса двух бойцов: " + differenceWeight + "кг!");
         var differenceWeight2 = weightFighter2 % weightFighter1;
-        System.out.println(differenceWeight2);
+        System.out.println("Разница веса двух бойцов: " + differenceWeight2 + "кг!");
     }
 }
