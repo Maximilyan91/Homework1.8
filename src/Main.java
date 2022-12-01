3491d993258678bd9f1f37041af4b254d4b8f138
Максим <gmax7911@gmail.com>
2022-12-01 19:59:26 +0300

    Сумма веса бойцов

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        var WeightFighter1 = 78.2;
+        var WeightFighter2 = 82.7;
+        var TotalWeight = WeightFighter1 + WeightFighter2;
+        System.out.println(TotalWeight);
+    }
+}
